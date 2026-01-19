759db91188169d09111bca072561ff7ce14f30dc
Sruthin31 <153161309+Sruthin31@users.noreply.github.com>
2026-01-19 14:21:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+function elem(n){
+let count = 0;
+let person = null;
+
+for(let i =0;i<n.length;i++){
+	if(person==0){
+		person = n[i];
+	}
+	if(n[i]===person){
+		count++;
+	}
+	else{
+		count--;
+	}
+}
+	return;
+}
